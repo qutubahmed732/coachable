2c983184de40f619a56b06c8c99cb71a03713ea9
Qutub Ahmed <qutubahmedkhan@outlook.com>
2025-09-29 23:03:23 +0500

    add vercel file

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import MainPage from "./Mainpage.jsx";
 import Thankyoupage from "./Thankyoupage.jsx";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 function App() {
 
   return (
     <>
-        <BrowserRouter>
+        <HashRouter>
           <Routes>
             <Route path="/" element={<MainPage />} />
             <Route path="/thank-you-page" element={<Thankyoupage />}></Route>
           </Routes>
-        </BrowserRouter>
+        </HashRouter>
     </>
   )
 }
